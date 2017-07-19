2b6ddfa1f94e3b89c891fbeba5593733cbe33de5
gyeongchan yun <gyeongchan.yun@treelogic.com>
2017-07-19 14:09:09 +0200

    practice string method

--- main_method.java
+++ main_method.java
@@ -1,7 +1,13 @@
 class MainMethod {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println(args[0]);
+		System.out.println("Hello World!"); //print "hello world!"
+		System.out.println(args[0]); //print args (command parameter)
+
+		String a = "Hello Java";
+		System.out.println(a.indexOf("Java"));
+		System.out.println(a.replaceAll("Java","python"));
+		System.out.println(a.substring(0,4));
+		System.out.println(a.toUpperCase());
 	}
 }
 
